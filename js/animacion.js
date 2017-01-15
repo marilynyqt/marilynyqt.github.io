9814d251328c3c3bca7d91b903058e5538e16460
marilynyqt <marilynyqt@hotmail.com>
2017-01-14 21:27:19 -0500

    animacion para la seccion de  inicio

--- js/animacion.js
+++ js/animacion.js
@@ -1,16 +1,3 @@
-$(function()
-  {
-	$('#a a')
-		.css( {backgroundPosition: "-20px 35px"} )
-		.mouseover(function(){
-			$(this).stop().animate({backgroundPosition:"(-20px 94px)"}, {duration:500})
-		})
-		.mouseout(function(){
-			$(this).stop().animate({backgroundPosition:"(40px 35px)"}, {duration:200, complete:function(){
-				$(this).css({backgroundPosition: "-20px 35px"})
-			}})
-		})
-    });
 (function() {
 
     var width, height, largeHeader, canvas, ctx, circles, target, animateHeader = true;
