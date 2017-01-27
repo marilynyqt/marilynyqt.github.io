94f246271096f36aca5cbaffcd70dd72e5d259c3
marilynyqt <marilynyqt@hotmail.com>
2017-01-27 13:20:11 -0500

    cambios

--- js/animacion.js
+++ js/animacion.js
@@ -233,3 +233,22 @@ demo.mousemove = function() {
 		for ( j = 0; j < max; j++ ) demo.spawn( touch.x, touch.y );
 	}
 };
+function quellueva() {
+     var image = document.getElementById('background');
+     image.onload = function() {
+          var engine = new RainyDay({
+               image: this,
+               blur: 10,
+               opacity: 5,
+               gravityAngle: Math.PI / 2,
+               gravityAngleVariance: 0
+          });
+          engine.gravity = engine.GRAVITY_NON_LINEAR;
+          engine.trail = engine.TRAIL_SMUDGE;
+          engine.rain([
+               [0, 3, 8],
+               [3, 8, 1]
+          ], 50);
+      };
+      image.crossOrigin = 'anonymous';
+}
